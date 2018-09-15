61a2f35b23266d847d9753d0415023eee3bccfa1
Rahul Chaudhary <7551493+rahulch95@users.noreply.github.com>
2018-09-14 19:15:24 -0700

    feat(config): added color and raw options to config get and config list

--- src/commands/config/list.ts
+++ src/commands/config/list.ts
@@ -5,11 +5,16 @@
 
 import command from '../../core/command';
 import * as config from '../../core/config';
+import * as opts from '../../core/options';
 import * as resources from '../../resources.json';
 
-export = command({
+export = command<{}, opts.color & opts.raw>({
     command: 'list',
     describe: resources.commands.config.commands.list.description,
+    options: {
+        ...opts.raw,
+        ...opts.color,
+    },
 
     handler(args, log) {
         log.json(config.list());
