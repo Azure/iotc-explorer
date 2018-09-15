030057b54801055903f22d03da66b46dcbd5f60a
Rahul Chaudhary <rachau@microsoft.com>
2018-09-14 19:06:31 -0700

    feat(config): added color and raw options to config

--- src/commands/config/get.ts
+++ src/commands/config/get.ts
@@ -5,14 +5,26 @@
 
 import command from '../../core/command';
 import * as config from '../../core/config';
+import CliError from '../../core/error';
+import * as opts from '../../core/options';
 import * as resources from '../../resources.json';
 
-export = command<{ key: string }>({
+export = command<{ key: string }, opts.color & opts.raw>({
     command: 'get <key>',
     describe: resources.commands.config.commands.get.description,
+    options: {
+        ...opts.raw,
+        ...opts.color,
+    },
 
     handler(args, log) {
         const value = config.getOverride(args.key as config.ConfigKey);
-        log.info(typeof value === 'string' ? value : JSON.stringify(value));
+        if (value === undefined) {
+            log.error(new CliError('KEY_NOT_FOUND', resources.commands.config.commands.get.errorNotFound));
+        } else if (typeof value === 'string') {
+            log.info(value);
+        } else {
+            log.json(value);
+        }
     }
 });
